cd74c73c6a30d108e417eb715b6bfda70bcf690e
Jun0613-spec <kumo5110@gmail.com>
2022-12-08 14:41:10 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,85 @@
+let shopItemsData = [{
+    id: "1st",
+    name: "Jameson Original",
+    price: 18,
+    desc:"Jameson Whiskey Original Bottle.",
+    img: "images/whiskey-1.jpg"
+}, 
+{
+    id: "2nd",
+    name: "Jack Daniels",
+    price: 20,
+    desc:"Jack Daniels Whiskey Original Bottle.",
+    img: "images/whiskey-2.jpg"
+}, 
+{
+    id: "3rd",
+    name: "Jim Beam",
+    price: 19,
+    desc:"Jim Beam Whiskey Original Bottle.",
+    img: "images/whiskey-3.jpg"
+}, 
+{
+    id: "4th",
+    name: "The Glenlivet",
+    price: 25,
+    desc:"The Glenlivet Whiskey Bottle.",
+    img: "images/whiskey-4.jpg"
+},
+{
+    id: "5th",
+    name: "Asahi",
+    price: 1.80,
+    desc:"330ml Bottle.",
+    img: "images/beer-1.jpg"
+},
+{
+    id: "6th",
+    name: "Budweiser",
+    price: 1.50,
+    desc:"330ml Bottle.",
+    img: "images/beer-2.jpg"
+},
+{
+    id: "7th",
+    name: "Budlight",
+    price: 1.40,
+    desc:"330ml Bottle.",
+    img: "images/beer-3.jpg"
+},
+{
+    id: "8th",
+    name: "Corona",
+    price: 1.80,
+    desc:"330ml Bottle.",
+    img: "images/beer-4.jpg"
+},
+{
+    id: "9th",
+    name: "Heineken",
+    price: 1.50,
+    desc:"330ml Bottle.",
+    img: "images/beer-5.jpg"
+},
+{
+    id: "10th",
+    name: "Peroni",
+    price: 1.50,
+    desc:"330ml Bottle.",
+    img: "images/beer-6.jpg"
+},
+{
+    id: "11th",
+    name: "Singha",
+    price: 1.80,
+    desc:"330ml Bottle.",
+    img: "images/beer-7.jpg"
+},
+{
+    id: "12th",
+    name: "Stella Artois",
+    price: 1.50,
+    desc:"330ml Bottle.",
+    img: "images/beer-8.jpg"
+},
+];
